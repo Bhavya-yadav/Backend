95bdd8015415610c7091897057236f66d050ea8b
Bhavya-yadav <bhavyayadav0402@gmail.com>
2026-06-13 22:18:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/google', (req,res) => {
+    res.send('Good Morning!')
+})
+
+app.get('/login', (req,res) => {
+    res.send('<h1>Login Page</h1>')
+})
+
+app.get('/signup', (req,res) => {
+    res.send('<h1>Signup Page</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
