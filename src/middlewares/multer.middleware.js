787f1d87f9ccc1be0ae8007338f30edd0aabf59f
Bhavya-yadav <bhavyayadav0402@gmail.com>
2026-06-22 19:47:36 +0530

    postman testing

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,13 +2,18 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/tmp')
+    cb(null, './public/temp')
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix +
-     path.extname(file.originalname))
+  
+  // Random file name so there is no name collision. 
+  // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+  // cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+  
+  cb(null, file.originalname)
   }
 })
 
-export const upload = multer({ storage : storage,  })
+export const upload = multer({ 
+  storage,  
+})
